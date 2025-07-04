7c5ce04768da7689ef1ab8545c6ce414f3003b1f
xworkzdev3@gmail.com <xworkzdev3@gmail.com>
2025-07-04 12:32:52 +0530

    yop and banners

--- src/components/constant/Urlconstant.js
+++ src/components/constant/Urlconstant.js
@@ -1,12 +1,12 @@
 export const Urlconstant = {
-  // url: "https://ombn.in/dream/",
-  // BACTH_URL: "https://ombn.in/attendance-batch/",
+  url: "https://ombn.in/dream/",
+  BACTH_URL: "https://ombn.in/attendance-batch/",
   // url: "https://ombn.in/Dream-pre/",
   // url: "http://103.174.102.136:8080/dream-dev/",
-  url: "http://localhost:8081/",
-  BACTH_URL: "http://localhost:8084/",
-  FEEDBACK_URL: "http://localhost:8085/",
-  spreadsheetId: "1NrTU01N_mU-HnPIQjPOzQhf_7CEXH430RzJb6wdDJ-Q",
-  // spreadsheetId: "1HI3nKYRbXNl2cu9rKwqKp3tznOZ7ziOupokU_daFZ8E", //production
+  // url: "http://localhost:8081/",
+  // BACTH_URL: "http://localhost:8084/",
+  // FEEDBACK_URL: "http://localhost:8085/",
+  // spreadsheetId: "1NrTU01N_mU-HnPIQjPOzQhf_7CEXH430RzJb6wdDJ-Q", //local
+  spreadsheetId: "1HI3nKYRbXNl2cu9rKwqKp3tznOZ7ziOupokU_daFZ8E", //production
   //spreadsheetId: "1BBvpbYuTvqaXQ2ooVItm0MGqB5XMnNgpHc0ZUW4zqLg" // pre-Production/
 };
