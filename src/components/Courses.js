e8e30c23899f0cffdb071ca9e8ce233d7497fad6
xworkzdev3@gmail.com <xworkzdev3@gmail.com>
2025-07-05 13:01:50 +0530

    mock changes

--- src/components/Courses.js
+++ src/components/Courses.js
@@ -1,115 +1,93 @@
 import React from "react";
-
 import "./Courses.css";
 
 const Courses = (props) => {
-	// const [CourseData , setCourseData ] = useState([])
+  const batches = props?.value?.Batches;
 
-	// useEffect(() => {
-	// 	axios.get("https://ombn.in/xworkz_api/getBatches")
-	// 	.then(res =>{
-	// 		setCourseData(res.data.Softwares)
+  // Handle loading or missing data
+  if (!batches || batches.length < 3) {
+    return (
+      <div className="course-page">
+        <h2>Loading courses...</h2>
+      </div>
+    );
+  }
 
-	// 	})
-	//   .catch(err => {
-	// 	console.log(err);
-	// 	axios.get("https://raw.githubusercontent.com/xworkzodc/JSON/master/Batches.json")
-	// 	  .then(res => {
-	// 		console.log("data getting from secondary source")
-	// 		setCourseData(res.data.Softwares);
-	// 	  })
-	// 	  .catch(err => {
-	// 		console.log(err);
-	// 	  });
-	//   })
-	//   },[]);
+  return (
+    <div>
+      <div className="course-page">
+        {/* Upcoming Courses */}
+        <div className="course-heading">
+          <h1>Upcoming Courses</h1>
+          <div className="courses-container">
+            {batches[0]?.Upcoming?.map((d, i) => (
+              <div className="course" key={i}>
+                <div className="course-preview">
+                  <h6>Course</h6>
+                  <h2>{d.courseName}</h2>
+                </div>
+                <div className="course-info">
+                  <h6>Trainer</h6>
+                  <h2>{d.facultyName}</h2>
+                  <h5>{d.type}</h5>
+                  <h6>Starts from</h6>
+                  <h6>{d.startDate}</h6>
+                  <h6>Location: {d.location}</h6>
+                  <h6>Timing: {d.time}</h6>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
 
-	return (
-		<div>
-			<div className="course-page">
-				<div className="course-heading">
-					<h1>Upcoming Courses</h1>
-					<div className="courses-container ">
-						{props?.value?.Batches[0]
-							? props?.value.Batches[0].Upcoming.map((d, i) => (
-									<div className="course">
-										<div className="course-preview">
-											<h6>Course</h6>
-											<h2>{d.courseName}</h2>
-											{/* <a href="#">
-												View all chapters <i className="fas fa-chevron-right" />
-											</a> */}
-										</div>
-										<div className="course-info">
-											<h6>Trainer</h6>
-											<h2>{d.facultyName}</h2>
-											<h5>{d.type}</h5>
-											<h6>starts from</h6>
-											<h6>{d.startDate}</h6>
-											<h6>Location: {d.location} </h6>
-											<h6>Timing: {d.time} </h6>
-										</div>
-									</div>
-							  ))
-							: "loading"}
-					</div>
-				</div>
-				<div className="course-heading">
-					<h1>Ongoing Courses</h1>
-					<div className="courses-container">
-						{props.value.Batches[1]
-							? props.value.Batches[1].Ongoing.map((d, i) => (
-									<div className="course">
-										<div className="course-preview">
-											<h6>Course</h6>
-											<h2>{d.courseName}</h2>
-											{/* <a href="#">
-												View all chapters <i className="fas fa-chevron-right" />
-											</a> */}
-										</div>
-										<div className="course-info">
-											<h6>Trainer</h6>
-											<h2>{d.facultyName}</h2>
-											<h5>{d.type}</h5>
-											<h6>started on</h6>
-											<h6>{d.startDate}</h6>
-											<h6>Location: {d.location} </h6>
-											<h6>Timings: {d.time} </h6>
-										</div>
-									</div>
-							  ))
-							: "loading"}
-					</div>
-				</div>
-				<div className="course-heading">
-					<h1>Completed Courses</h1>
+        {/* Ongoing Courses */}
+        <div className="course-heading">
+          <h1>Ongoing Courses</h1>
+          <div className="courses-container">
+            {batches[1]?.Ongoing?.map((d, i) => (
+              <div className="course" key={i}>
+                <div className="course-preview">
+                  <h6>Course</h6>
+                  <h2>{d.courseName}</h2>
+                </div>
+                <div className="course-info">
+                  <h6>Trainer</h6>
+                  <h2>{d.facultyName}</h2>
+                  <h5>{d.type}</h5>
+                  <h6>Started on</h6>
+                  <h6>{d.startDate}</h6>
+                  <h6>Location: {d.location}</h6>
+                  <h6>Timings: {d.time}</h6>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
 
-					<div className="courses-container ">
-						{props.value.Batches[2]
-							? props.value.Batches[2].Completed.map((d, i) => (
-									<div className="course">
-										<div className="course-preview">
-											<h6>Course</h6>
-											<h2>{d.courseName}</h2>
-											{/* <a href="#">
-												View all chapters <i className="fas fa-chevron-right" />
-											</a> */}
-										</div>
-										<div className="course-info">
-											<h6>Trainer</h6>
-											<h2>{d.facultyName}</h2>
-											<h5>{d.type}</h5>
-											<h6>starts from</h6>
-											<h6>{d.startDate}</h6>
-										</div>
-									</div>
-							  ))
-							: "loading"}
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+        {/* Completed Courses */}
+        <div className="course-heading">
+          <h1>Completed Courses</h1>
+          <div className="courses-container">
+            {batches[2]?.Completed?.map((d, i) => (
+              <div className="course" key={i}>
+                <div className="course-preview">
+                  <h6>Course</h6>
+                  <h2>{d.courseName}</h2>
+                </div>
+                <div className="course-info">
+                  <h6>Trainer</h6>
+                  <h2>{d.facultyName}</h2>
+                  <h5>{d.type}</h5>
+                  <h6>Completed on</h6>
+                  <h6>{d.startDate}</h6>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Courses;
