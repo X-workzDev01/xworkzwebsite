b3e322c2d619883397fed1904aa6e6a07ef6c562
LaluprasadXworkz <laluprasadm.xworkz@gmail.com>
2025-07-15 12:30:45 +0530

    15-07-2025 website changes

--- src/components/constant/Urlconstant.js
+++ src/components/constant/Urlconstant.js
@@ -1,21 +1,23 @@
 export const Urlconstant = {
   //Production
-   url: "https://ombn.in/dream/",
-   BACTH_URL: "https://ombn.in/attendance-batch/",
-   url: "https://ombn.in/Dream-pre/",
-   url: "http://103.174.102.136:8080/dream-dev/",
-   spreadsheetId: "1HI3nKYRbXNl2cu9rKwqKp3tznOZ7ziOupokU_daFZ8E", //production
+  // url: "https://ombn.in/dream/",
+  // BACTH_URL: "https://ombn.in/attendance-batch/",
+  // FEEDBACK_URL: "https://ombn.in/feedback-mock/"
+  // spreadsheetId: "1HI3nKYRbXNl2cu9rKwqKp3tznOZ7ziOupokU_daFZ8E",
 
 
   //pre-Production/
-//  url: "http://103.174.102.136:8080/dream-pre/",
+ //url: "http://103.174.102.136:8080/dream-pre/",
 //  BACTH_URL: "http://103.174.102.136:8080/attendance-batch-pre/",
 //  FEEDBACK_URL: "http://103.174.102.136:8080/feedback-mock-pre/",
 //  spreadsheetId: "1BBvpbYuTvqaXQ2ooVItm0MGqB5XMnNgpHc0ZUW4zqLg"
 
-  //dev
-  // url: "http://localhost:8081/",
-  // BACTH_URL: "http://localhost:8084/",
-  // FEEDBACK_URL: "http://localhost:8085/",
-  // spreadsheetId: "1NrTU01N_mU-HnPIQjPOzQhf_7CEXH430RzJb6wdDJ-Q",
+   //  dev
+  // url: "http://103.174.102.136:8080/dream-dev/",
+
+  //local
+   url: "http://localhost:8081/",
+   BACTH_URL: "http://localhost:8084/",
+   FEEDBACK_URL: "http://localhost:8085/",
+   spreadsheetId: "1NrTU01N_mU-HnPIQjPOzQhf_7CEXH430RzJb6wdDJ-Q",
 };
